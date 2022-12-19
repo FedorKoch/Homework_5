67bb73ed5d8d5ffd0d7a1ff71f3392702ac56d91
Fedor <fedor.kochkin48@gmail.com>
2022-12-19 23:16:25 +0300

    решение задачи 36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int sum = 0;
+Random rnd = new Random();
+Console.WriteLine("введите количество элементов массива");
+int a = int.Parse(Console.ReadLine());
+int[] mas = new int[a];
+for (int i = 0; i < mas.Length; i++)
+{
+mas[i] = rnd.Next(-100,100); // Указан диапазон для большей наглядности в выводе, на решение задачи не влияет
+Console.Write("{0} ", mas[i]);
+if (i % 2 != 0)
+sum = sum + mas[i];
+}
+Console.WriteLine();
+Console.WriteLine("Сумма лементов, стоящих на нечётных позициях равна");
+Console.WriteLine(sum);
